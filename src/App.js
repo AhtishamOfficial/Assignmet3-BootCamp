228c6f0d194c0097573facbf80239dadae19d05f
AhtishamOfficial <aliahtishamofficial@gmail.com>
2022-08-19 00:05:02 +0500

    Initial-Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import About from "./Components/About/About";
+import Home from "./Components/Home/Home";
+import Portfolio from "./Components/Portfolio/Portfolio";
+
+function App() {
+  return (
+    <>
+    <Home />
+    <About />
+    <Portfolio />
+    </>
+  );
+}
+
+export default App;
