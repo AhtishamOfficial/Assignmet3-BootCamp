6174346b7caabec98856146d8f14679b3137c666
AhtishamOfficial <aliahtishamofficial@gmail.com>
2022-08-20 13:51:49 +0500

    Errors-Recovery

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
 import Home from "./Components/Home/Home";
 import Portfolio from "./Components/Portfolio/Portfolio";
+import Testimonial from "./Components/Testimonial/Testimonial";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <Home />
     <About />
     <Portfolio />
+   <Testimonial/>
+  <Contact />
     </>
   );
 }
